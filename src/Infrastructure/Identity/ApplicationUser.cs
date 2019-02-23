34eab69be876304349eaee05cec80785ba93c083
kaldren <drenski666@gmail.com>
2019-02-23 21:04:06 +0200

    Added identity to Infrastructure

---
+++ src/Infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AskGoo2.Infrastructure.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [PersonalData]
+        public string FirstName { get; set; }
+        [PersonalData]
+        public string LastName { get; set; }
+    }
+}
