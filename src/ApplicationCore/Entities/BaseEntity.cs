ed0c9985cabcfa66288dfeef1ed6dd967a932135
kaldren <drenski666@gmail.com>
2019-02-23 21:45:05 +0200

    Added Question entity

---
+++ src/ApplicationCore/Entities/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AskGoo2.ApplicationCore.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ src/ApplicationCore/Entities/QuestionAggregate/Question.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AskGoo2.ApplicationCore.Entities.QuestionAggregate
+{
+    public class Question : BaseEntity
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
